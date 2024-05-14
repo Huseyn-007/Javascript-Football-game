384135f6f852dcb0815f0798f65473fcfe8b2a9c
Huseyn Huseynov <131697602+HuseynGPT@users.noreply.github.com>
2024-05-14 19:45:29 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+
+const area = document.getElementById("area");
+
+const step = 10;
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", event => {
+  console.log(event.key);
+});
